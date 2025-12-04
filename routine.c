de4fad4bf4c2365e542d015e4cbc2daf3b9f6d31
hugo-miguel07 <hugomiguel.cacao@gmail.com>
2025-12-04 00:09:40 +0000

    estruturar a routine com comentários logicos sobre o que fazer

--- routine.c
+++ routine.c
@@ -14,26 +14,49 @@
 
 void	sleeping()
 {
-	
+	//libertas os forks
+	//usleep do tempo de dormir
+	//enviar sinal que está a dormir	
 }
 
 void	eating()
 {
-	
+	//aceder aos forks
+	//usleep do tempo de comer
+	//enviar sinal que está a comer
 }
 
 void	thinking()
 {
-	
+	//enviar sinal que está a pensar
+	usleep(100);
 }
 
 void	*routine(void *philosopher)
 {
-	t_philosopher *phil;
+	t_philosopher	*phil;
+	int				times_to_eat;
 
 	phil = (t_philosopher *)philosopher;
 	if (!phil)
 		return (NULL);
-	//make the moafuckas routine
+	//ideia depois para os prints:
+	//	a struct phil pode ter mais 1 variavel
+	//	que representa o estado em que se encontram.
+	//	esse estado vai ser atualizado e printado quando
+	//	entrarem noutra função de execução
+	//	(thinking, eating, sleeping)
+	times_to_eat = phil->eat_max_num;
+	while (times_to_eat)
+	{
+		thinking();
+		eating();
+		if (times_to_eat > 0)
+			times_to_eat--;
+		if (times_to_eat == 0)
+			break ;
+		sleeping();
+	}
+	//flag para acabar todas as outras threads
 	return (NULL);
 }
